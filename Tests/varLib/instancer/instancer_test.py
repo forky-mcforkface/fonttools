38eb2af1bcffb4356bdaa843afcf34265513a0d6
Behdad Esfahbod <behdad@behdad.org>
2022-08-09 14:57:14 -0600

    [instancer] Fix more tests

--- Tests/varLib/instancer/instancer_test.py
+++ Tests/varLib/instancer/instancer_test.py
@@ -1908,12 +1908,7 @@ def test_parseLimits_invalid(limits):
 
 def test_normalizeAxisLimits_tuple(varfont):
     normalized = instancer.normalizeAxisLimits(varfont, {"wght": (100, 400)})
-    assert normalized == {"wght": (-1.0, 0)}
-
-
-def test_normalizeAxisLimits_unsupported_range(varfont):
-    with pytest.raises(NotImplementedError, match="Unsupported range"):
-        instancer.normalizeAxisLimits(varfont, {"wght": (401, 700)})
+    assert normalized == {"wght": (-1.0, 0, 0)}
 
 
 def test_normalizeAxisLimits_no_avar(varfont):
@@ -1921,7 +1916,7 @@ def test_normalizeAxisLimits_no_avar(varfont):
 
     normalized = instancer.normalizeAxisLimits(varfont, {"wght": (400, 500)})
 
-    assert normalized["wght"] == pytest.approx((0, 0.2), 1e-4)
+    assert normalized["wght"] == pytest.approx((0, 0, 0.2), 1e-4)
 
 
 def test_normalizeAxisLimits_missing_from_fvar(varfont):
